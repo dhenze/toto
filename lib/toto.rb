e655e1c63cc847f686d5dc77c1a2c964d7d9bc97
joustava <joustava@gmail.com>
2013-08-15 21:24:41 +0200

    Fix coderay plugin not found issue while scanning

--- lib/toto.rb
+++ lib/toto.rb
@@ -31,7 +31,7 @@ module Toto
 
   class CodeRayify < Redcarpet::Render::HTML
     def block_code(code, language)
-      CodeRay.scan(code, language).div
+      CodeRay.highlight(code, language)
     end
   end
 
